3c68a5d1c8d9031db242468041713e8c33893b7d
VedantY17 <vedant.ny@gmail.com>
2024-03-06 23:35:29 +0530

    json data retrival

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 
-export default function Card() {
+export default function Card(props) {
+
   return (
     <div>
-      <div className="card mt-3" style={{ width: "18rem", maxHeight: "360px" }}>
+      <div className="card mt-3" style={{ width: "16rem", maxHeight: "500px" }}>
         <img
-          src="https://source.unsplash.com/random/?biryani"
+          src={props.imgSrc}
           className="card-img-top"
           alt="..."
-          style={{ maxHeight: "220px" }}
+          style={{ height:"220px", objectFit:"fill" }}
         />
         <div className="card-body">
-          <p className="card-text">Some important text</p>
+          <h5 className="card-title">{props.phoneName}</h5>
+          <p className="card-text">{props.modelName}</p>
+          <h6 className="d-inline">Storage: {props.storage}</h6>
+          <h6 className="d-inline ml-4">Price: {props.price}</h6>
           <div className="container w-100">
-            <select className="m-2 h-100 rounded">
-              {Array.from(Array(6), (e, i) => {
+            <select className="mt-2 h-100 rounded">
+              {Array.from(Array(6), (e, i) => { 
                 return (
                   <option key={i + 1} value={i + 1}>
                     {" "}
@@ -22,10 +26,6 @@ export default function Card() {
                   </option>
                 );
               })}
-            </select>
-            <select className="m-2 h-100 rounded">
-              <option value="half">Half</option>
-              <option value="full">Full</option>
             </select>
             <div className="d-inline fs-5">Total price</div>
           </div>
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Card from "../components/Card";
-import Carousel from "../components/Carousel";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 export default function Home() {
 
+  const [search, setSearch] = useState('');
   const [phoneCat, setPhoneCat] = useState([]);
   const [phones, setPhones] = useState([]);
 
@@ -31,17 +31,81 @@ export default function Home() {
     loadData()
   }, [])
 
-
-
-
-
-
-
-
   return (
     <div>
       <div><Navbar /></div>
-      <div><Carousel /></div>
+      <div>
+        <div
+          id="carouselExampleControls"
+          className="carousel slide"
+          data-ride="carousel"
+        >
+          <div className="carousel-inner" id="carousel-image">
+            <div className="carousel-caption" style={{ zIndex: "10" }}>
+              <div className="d-flex justify-content-center">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  value={search}
+                  onChange={(e)=>{setSearch(e.target.value)}}
+                />
+                {/* <button className="btn btn-outline-success" type="submit">
+                  Search
+                </button> */}
+              </div>
+            </div>
+
+            <div className="carousel-item active">
+              <img
+                className="d-block w-100"
+                src="https://source.unsplash.com/random/1000×700/?smartphones"
+                alt="First slide"
+                style={{ filter: "brightness(50%)" }}
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                className="d-block w-100"
+                src="https://source.unsplash.com/random/1000×700/?mobilephones"
+                alt="second slide"
+                style={{ filter: "brightness(50%)" }}
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                className="d-block w-100"
+                src="https://source.unsplash.com/random/1000×700/?phones"
+                alt="Third slide"
+                style={{ filter: "brightness(50%)" }}
+              />
+            </div>
+          </div>
+          <a
+            className="carousel-control-prev"
+            href="#carouselExampleControls"
+            role="button"
+            data-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+          </a>
+          <a
+            className="carousel-control-next"
+            href="#carouselExampleControls"
+            role="button"
+            data-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+          </a>
+        </div>
+      </div>
       <div className="container">
         {
           phoneCat !== []
@@ -53,11 +117,16 @@ export default function Home() {
                 <hr />
                 {phones !== []
                   ?
-                  phones.filter((item) => item.CategoryName === data.CategoryName)
+                  phones.filter((item) => (item.CategoryName === data.CategoryName) && item.Brand.toLowerCase().includes(search.toLocaleLowerCase()))
                     .map(filterItems => {
                       return (
                         <div key={filterItems._id} className="col-12 col-md-6 col-lg-3">
-                          <Card></Card>
+                          <Card phoneName={filterItems.Brand}
+                            modelName={filterItems.Model}
+                            storage={filterItems.Options[0].Storage} // Accessing Storage from Options array
+                            price={filterItems.Options[0].Price} // Accessing Price from Options array
+                            imgSrc={filterItems.Img}
+                          ></Card>
                         </div>
                       )
                     }
